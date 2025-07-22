5411b740fc5d285b4b5e16b28a4bace72a7979d7
varsha1206 <teamleoeaton@gmail.com>
2025-07-22 21:18:51 +0200

    Editing Frontend display

--- backend/see.py
+++ backend/see.py
@@ -2,10 +2,10 @@ import db_utilities
 from datetime import datetime
 
 # #DROP
-with db_utilities.cursor_handler(db_utilities.con) as cur:
-    db_utilities.db_select(cur,"DROP TABLE EXPENSES")
-    print("Dropped table")
+# with db_utilities.cursor_handler(db_utilities.con) as cur:
+#     db_utilities.db_select(cur,"DROP TABLE EXPENSES")
+#     print("Dropped table")
     
 #SELECT
-# with db_utilities.cursor_handler(db_utilities.con) as cur:
-#     print(db_utilities.db_select(cur,"SELECT * FROM EXPENSES"))
+with db_utilities.cursor_handler(db_utilities.con) as cur:
+    print(db_utilities.db_select(cur,"SELECT * FROM EXPENSES"))
